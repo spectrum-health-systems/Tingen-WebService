b1d7cdc57c2a46e2eb8931750352084364f23491
Chris Banwarth <github@aprettycoolprogram.com>
2026-07-27 08:43:11 -0400

    Refresh API documentation

--- src/ProjectInfo.cs
+++ src/ProjectInfo.cs
@@ -5,7 +5,7 @@
 // Licensed under the Apache 2.0 license.
 // =============================================================================
 
-// R26.7-development+b27.0808
+// R26.7-development+b27.0840
 
 namespace TingenWebService
 {
