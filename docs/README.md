30177e87ca0c1c46b1b6206785323a357d320421
Chris Banwarth <github@aprettycoolprogram.com>
2026-08-19 14:30:18 -0400

    Update documentation

--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,7 @@
-<div align="center">
+
   <img src="https://github.com/spectrum-health-systems/Tingen-WebService/blob/development/.github/logo/Tingen-WebService-256x173.png" alt="Tingen Web Service">
   <h1>Documentation</h1>
-</div>
+
 
 * [Manual](man/README.md)  
 * [API Documentation](https://spectrum-health-systems.github.io/Tingen-WebService/api/html/Welcome.htm)  
--- docs/api/scripts/presentationStyle.js
+++ docs/api/scripts/presentationStyle.js
@@ -299,7 +299,7 @@ function LoadToc()
     const selectedTopicId = $("meta[name='guid']").attr("content");
 
     $.ajax({
-        url: "../toc.json?v=C026314A",
+        url: "../toc.json?v=5AEA538E",
         dataType: "json",
         success: function (data)
         {
@@ -497,7 +497,7 @@ function PerformSearch()
         searchResults.innerHTML = "Loading index...";
 
         $.ajax({
-            url: "searchIndex.json?v=C026314A",
+            url: "searchIndex.json?v=5AEA538E",
             dataType: "json",
             success: function (data)
             {
@@ -515,7 +515,7 @@ function PerformSearch()
     if(!fileIndex)
     {
         $.ajax({
-            url: "fileIndex.json?v=C026314A",
+            url: "fileIndex.json?v=5AEA538E",
             dataType: "json",
             success: function (data)
             {
