fe546b72b7a13e76d78580c0daa50929917e29ef
Chris Banwarth <github@aprettycoolprogram.com>
2026-09-22 09:58:54 -0400

    Update API documentation

--- docs/api/scripts/presentationStyle.js
+++ docs/api/scripts/presentationStyle.js
@@ -299,7 +299,7 @@ function LoadToc()
     const selectedTopicId = $("meta[name='guid']").attr("content");
 
     $.ajax({
-        url: "../toc.json?v=BBB9C7DC",
+        url: "../toc.json?v=16DF549",
         dataType: "json",
         success: function (data)
         {
@@ -497,7 +497,7 @@ function PerformSearch()
         searchResults.innerHTML = "Loading index...";
 
         $.ajax({
-            url: "searchIndex.json?v=BBB9C7DC",
+            url: "searchIndex.json?v=16DF549",
             dataType: "json",
             success: function (data)
             {
@@ -515,7 +515,7 @@ function PerformSearch()
     if(!fileIndex)
     {
         $.ajax({
-            url: "fileIndex.json?v=BBB9C7DC",
+            url: "fileIndex.json?v=16DF549",
             dataType: "json",
             success: function (data)
             {
